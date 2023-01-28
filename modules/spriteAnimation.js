81b9530b004e547473388d8bce1afe859026332c
Oleksandr <lanko.oleksandr@gmail.com>
2023-01-28 13:53:01 +0200

    sprite animation tower

--- modules/spriteAnimation.js
+++ modules/spriteAnimation.js
@@ -13,7 +13,8 @@ const frameTime = 1000 / 30;
 
 
 const test = new Image();
-test.src = "img/test.png";
+test.src = "../img/test.png";
+// размеры юнитов в спрайте
 const spriteWidth = 50;
 const spriteHeight = 50;
 let frameX = 0;
@@ -21,31 +22,50 @@ let frameY = 0;
 let gameFrame = 0;
 const frameCount = 10;
 
+
 const redTowerSprite = new Image ();
 redTowerSprite.src = "../img/red_tower.png";
 
 const blueTowerSprite = new Image ();
 blueTowerSprite.src = "../img/blue_tower.png";
+// размеры башен в спрайте
+const spriteWidthTower = 198;
+const spriteHeightTower = 450;
+
+//функція для відображення спрайтів обовязково використовувати кількість кадрів в рядку і вертикально або горизонталдьно
+// послідосвність рядків
+function drawSprite (img, sX, sY, sW, sH, dX, dY, dW, dH, colSprites, rowSprites ) {
+        if (rowSprites === 'vertical') {
+            ctx.drawImage(img, sX, sY * sH, sW, sH, dX, dY, dW, dH);
+        }
+        if (rowSprites === 'horizontal') {
+            ctx.drawImage(img, sX * sW, sY, sW, sH, dX, dY, dW, dH);
+        }
+}
 
 function draw() {
     ctx.clearRect(0, 0, widthCtx, heightCtx);
-    ctx.drawImage(redTowerSprite, 0, 0, 190, 450, 0, 100, 200, 450);
-    ctx.drawImage(blueTowerSprite, 0, 0, 190, 450, 600, 115, 200, 450);
-    ctx.drawImage(test, 0, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 560, 50, 50);
-    ctx.drawImage(test, 50 , frameY * spriteHeight, spriteWidth, spriteHeight, 50, 560, 50, 50);
-    ctx.drawImage(test, 100, frameY * spriteHeight, spriteWidth, spriteHeight, 100, 560, 50, 50);
-    ctx.drawImage(test, 150, frameY * spriteHeight, spriteWidth, spriteHeight, 150, 560, 50, 50);
-    ctx.drawImage(test, 200, frameY * spriteHeight, spriteWidth, spriteHeight, 200, 560, 50, 50);
+    drawSprite(test, 0, frameY, spriteWidth, spriteHeight, 100, 560, 50, 50, 3 , 'vertical');
+    drawSprite(redTowerSprite, frameX, frameY, spriteWidthTower, spriteHeightTower, 0, 150, 100, 450, 5, 'horizontal');
+    drawSprite(blueTowerSprite, frameX, frameY, spriteWidthTower, spriteHeightTower, 900, 150, 100, 450, 5, 'horizontal');
+    drawSprite(test, 1* spriteWidth, frameY, spriteWidth, spriteHeight, 150, 560, 50, 50, 3 , 'vertical');
+    drawSprite(test, 2* spriteWidth, frameY, spriteWidth, spriteHeight, 200, 560, 50, 50, 3 , 'vertical');
+    drawSprite(test, 3* spriteWidth, frameY, spriteWidth, spriteHeight, 250, 560, 50, 50, 3 , 'vertical');
+    drawSprite(test, 4* spriteWidth, frameY, spriteWidth, spriteHeight, 300, 560, 50, 50, 3 , 'vertical');
+    console.log(gameFrame);
     if (gameFrame % frameCount == 0) {
         frameY++;
-        if (frameY > 3) {
+        frameX++;
+        if (frameY > 2) {
             frameY = 0;
+        }
+        if (frameX > 4) {
+            frameX = 0;
         }
     }
     gameFrame++;
 
-    
-    requestAnimationFrame(() => 
+    requestAnimationFrame(() =>
         setTimeout(draw, frameTime));
 }
 
